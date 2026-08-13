767d062648bd9bbce81c4a86d5236c0d61866976
Andrea <andreamandrici@users.noreply.github.com>
2026-08-13 16:20:53 +0200

    Create import_2026.sql

---
+++ import/import_2026.sql
@@ -0,0 +1,24 @@
+-----------------------------------------------------------------------------
+DROP SCHEMA IF EXISTS foreign_data CASCADE;
+CREATE SCHEMA foreign_data;
+-----------------------------------------------------------------------------
+DROP SERVER IF EXISTS species_iucn_spatial_202607_fdw CASCADE;
+CREATE SERVER species_iucn_spatial_202607_fdw
+  FOREIGN DATA WRAPPER ogr_fdw
+  OPTIONS (
+	datasource '/data/swap/species_2026/spatial',
+	format 'ESRI Shapefile' );
+------------------------------------------------------------------------------------------------------
+IMPORT FOREIGN SCHEMA ogr_all FROM SERVER species_iucn_spatial_202607_fdw INTO foreign_data;
+-------------------------------------------------------------------------------------------------------
+DO
+$$
+DECLARE
+    tbname name;
+BEGIN
+   FOR tbname IN SELECT foreign_table_name::text FROM information_schema._pg_foreign_tables WHERE foreign_table_schema::text = 'foreign_data' LOOP
+        RAISE NOTICE 'CREATING TABLE : species_2026_input_data_original.%', tbname;
+        EXECUTE format('CREATE TABLE species_2026_input_data_original.%I AS SELECT * FROM foreign_data.%I;', tbname,tbname);
+   END LOOP;
+END;
+$$ LANGUAGE plpgsql;
