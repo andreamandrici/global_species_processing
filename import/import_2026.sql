238ef07ccec9ecdac2233d8be5bbc167ccbfa977
Andrea <andreamandrici@users.noreply.github.com>
2026-08-14 10:16:01 +0200

    Update import_2026.sql

--- import/import_2026.sql
+++ import/import_2026.sql
@@ -242,19 +242,112 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 -------------------------------------------------------------------------------------------------------
-
------- TO DO HERE!
-
+----------------------------------------------------------------------------
+CREATE TABLE species_2026_input_data_original.nsp_all_other_fields AS 
+SELECT * FROM foreign_data.all_other_fields_1 
+UNION 
+SELECT * FROM foreign_data.all_other_fields_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_assessments AS 
+SELECT * FROM foreign_data.assessments_1 
+UNION 
+SELECT * FROM foreign_data.assessments_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_assessments_with_html AS 
+SELECT * FROM foreign_data.assessments_with_html_1 
+UNION 
+SELECT * FROM foreign_data.assessments_with_html_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_common_names AS 
+SELECT * FROM foreign_data.common_names_1 
+UNION 
+SELECT * FROM foreign_data.common_names_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_conservation_needed AS 
+SELECT * FROM foreign_data.conservation_needed_1 
+UNION 
+SELECT * FROM foreign_data.conservation_needed_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_countries AS 
+SELECT * FROM foreign_data.countries_1 
+UNION 
+SELECT * FROM foreign_data.countries_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_credits AS 
+SELECT * FROM foreign_data.credits_1 
+UNION 
+SELECT * FROM foreign_data.credits_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_dois AS 
+SELECT * FROM foreign_data.dois_1 
+UNION 
+SELECT * FROM foreign_data.dois_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_fao AS 
+SELECT * FROM foreign_data.fao_1 
+UNION 
+SELECT * FROM foreign_data.fao_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_habitats AS 
+SELECT * FROM foreign_data.habitats_1 
+UNION 
+SELECT * FROM foreign_data.habitats_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_lme AS 
+SELECT * FROM foreign_data.lme_1 
+UNION 
+SELECT * FROM foreign_data.lme_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_references_ AS 
+SELECT * FROM foreign_data.references_1 
+UNION 
+SELECT * FROM foreign_data.references_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_research_needed AS 
+SELECT * FROM foreign_data.research_needed_1 
+UNION 
+SELECT * FROM foreign_data.research_needed_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_simple_summary AS 
+SELECT * FROM foreign_data.simple_summary_1 
+UNION 
+SELECT * FROM foreign_data.simple_summary_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_synonyms AS 
+SELECT * FROM foreign_data.synonyms_1 
+UNION 
+SELECT * FROM foreign_data.synonyms_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_taxonomy AS 
+SELECT * FROM foreign_data.taxonomy_1 
+UNION 
+SELECT * FROM foreign_data.taxonomy_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_taxonomy_with_html AS 
+SELECT * FROM foreign_data.taxonomy_with_html_1 
+UNION 
+SELECT * FROM foreign_data.taxonomy_with_html_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_threats AS 
+SELECT * FROM foreign_data.threats_1 
+UNION 
+SELECT * FROM foreign_data.threats_2 
+;
+CREATE TABLE species_2026_input_data_original.nsp_usetrade AS 
+SELECT * FROM foreign_data.usetrade_1 
+UNION 
+SELECT * FROM foreign_data.usetrade_2 
+;
+-------------------------------------------------------------------------------------
+CREATE TABLE species_2026_input_data_original.nsp_endemics AS
+SELECT DISTINCT internaltaxonid::integer,scientificname::text
+FROM foreign_data.endemics_simple_summary_0
+ORDER BY internaltaxonid::integer;
 -------------------------------------------------------------------------------------------------------
 DROP SCHEMA foreign_data CASCADE;
 CREATE SCHEMA foreign_data;
 -------------------------------------------------------------------------------------------------------
 DROP SERVER IF EXISTS species_iucn_non_spatial_1_202607_fdw CASCADE;
 DROP SERVER IF EXISTS species_iucn_non_spatial_2_202607_fdw CASCADE;
-DROP SERVER IF EXISTS species_iucn_non_spatial_3202607_fdw CASCADE;
--------------------------------------------------------------------------------------------------------
-CREATE TABLE species_2026.spatial_sharks_rays_chimaeras AS
-SELECT
-fid,geom,id_no,sci_name::text,presence,origin,seasonal,compiler::text,yrcompiled,citation::text,subspecies::text,subpop::text,source::text,island::text,tax_comm::text,dist_comm::text,generalisd,legend::text,kingdom::text,phylum::text,class::text,order_::text,family::text,genus::text,category::text,marine::text,terrestria::text,freshwater::text,shape_leng,shape_area
-FROM species_2026_input_data_original.sharks_rays_chimaeras WHERE presence IN (1) AND origin IN (1,2,6) AND seasonal IN (1,2,3)
-ORDER BY id_no;
+DROP SERVER IF EXISTS species_iucn_non_spatial_3_202607_fdw CASCADE;
+
