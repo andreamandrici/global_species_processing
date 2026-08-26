bc04416db9419f7062cdbbd474dff324b236c8f8
Andrea <andreamandrici@users.noreply.github.com>
2026-08-26 12:48:07 +0200

    Update range_sqkm_2026_03_check.sql

--- spatial/range_sqkm_2026_03_check.sql
+++ spatial/range_sqkm_2026_03_check.sql
@@ -1,6 +1,25 @@
+DROP TABLE IF EXISTS species_2026.species_ranges_check_by_years;
+CREATE TABLE species_2026.species_ranges_check_by_years AS
 WITH
-a AS (SELECT o,id_no,taxon,binomial,range_sqkm range25 FROM species_2025.dopa_species),
-b AS (SELECT o,id_no,taxon,binomial,range_sqkm range26 FROM species_2026.dopa_species),
-c AS (SELECT * FROM a FULL OUTER JOIN b USING(o,id_no,taxon,binomial) ORDER BY o,id_no),
-d AS (SELECT *,ROUND(range25::numeric/range26::numeric*100::numeric,2) rr FROM c)
-SELECT * FROM d WHERE rr < 90 or rr > 100
+a1 AS (
+	SELECT o,id_no FROM species_2026.species_ranges_previous_years
+	UNION
+	SELECT o,id_no FROM species_2025.dopa_species
+	UNION
+	SELECT o,id_no FROM species_2026.dopa_species
+	),
+y23 AS (SELECT o,id_no,range_sqkm ry23 FROM species_2026.species_ranges_previous_years WHERE y=2023),
+y24 AS (SELECT o,id_no,range_sqkm ry24 FROM species_2026.species_ranges_previous_years WHERE y=2024),
+y25 AS (SELECT o,id_no,range_sqkm ry25 FROM species_2025.dopa_species),
+y26 AS (SELECT o,id_no,range_sqkm ry26 FROM species_2026.dopa_species),
+tot AS (
+SELECT *
+FROM a1
+LEFT JOIN y23 USING(o,id_no)
+LEFT JOIN y24 USING(o,id_no)
+LEFT JOIN y25 USING(o,id_no)
+LEFT JOIN y26 USING(o,id_no)
+ORDER BY o,id_no),
+tot_perc AS (SELECT *,ROUND(ry26::numeric/ry25::numeric*100::numeric,2) ry_26_25_perc FROM tot)
+SELECT * FROM tot_perc;
+SELECT * FROM species_2026.species_ranges_check_by_years WHERE ry_26_25_perc < 90 or ry_26_25_perc > 110;
