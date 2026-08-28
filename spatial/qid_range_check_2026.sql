148999198fe3c600d9ce8e6b45a4f0a2c65cf5e9
Andrea <andreamandrici@users.noreply.github.com>
2026-08-28 13:33:33 +0200

    Update qid_range_check_2026.sql

--- spatial/qid_range_check_2026.sql
+++ spatial/qid_range_check_2026.sql
@@ -13,3 +13,5 @@ ROUND((ry26::numeric/ry23::numeric*100::numeric),2) c23
 FROM species_2026.species_ranges_check_by_years JOIN a USING(id_no)
 ORDER BY o,lost_qids DESC,id_no ;
 SELECT * FROM selected_species_range_qid;
+CREATE TABLE species_2026_reptiles.reptiles_tmp_check AS (SELECT * FROM species_2026_reptiles.h_flat WHERE reptiles && '{6494}');
+CREATE TABLE species_2026_mammals.mammals_tmp_check AS (SELECT * FROM species_2026_mammals.h_flat WHERE mammals && '{2477}');
